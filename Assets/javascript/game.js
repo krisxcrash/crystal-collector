e89737b91af2e16bf4ebfa81e988280168547312
Kristine Martin <kristinemartin@Kristines-MacBook-Pro.local>
2017-07-04 17:12:21 -0700

    updated html and css, added javascript for crystal values and random numbers

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -17,3 +17,83 @@
 	- +1 POINT FOR LOSSES
 */
 
+
+$(document).ready(function() {
+
+	var randomNumber = Math.floor(Math.random() * ((120 - 19)+1) + 19);
+	var userNumber = 0;
+	var wins = 0;
+	var losses = 0;
+	var gem1 = Math.floor(Math.random() * ((19 - 1)+1) + 1);
+	var gem2 = Math.floor(Math.random() * ((19 - 1)+1) + 1);
+	var gem3 = Math.floor(Math.random() * ((19 - 1)+1) + 1);
+	var gem4 = Math.floor(Math.random() * ((19 - 1)+1) + 1); 
+	var gem5 = Math.floor(Math.random() * ((19 - 1)+1) + 1);
+	var gem6 = Math.floor(Math.random() * ((19 - 1)+1) + 1);
+
+
+// WHEN CLICKED, "USER NUMBER" INCREASES BY VALUE OF GEM
+	$("#gem1").on("click", function() {
+		console.log(gem1);
+		userNumber++;
+	});
+
+	$("#gem2").on("click", function() {
+		console.log(gem2);
+		userNumber++;
+	});
+
+	$("#gem3").on("click", function() {
+		console.log(gem3);
+		userNumber++;
+	});
+
+	$("#gem4").on("click", function() {
+		console.log(gem4);
+		userNumber++;
+	});
+
+	$("#gem5").on("click", function() {
+		console.log(gem5);
+		userNumber++;
+	});
+
+	$("#gem6").on("click", function() {
+		console.log(gem6);
+		userNumber++;
+	});
+
+
+function displayRandom() {
+
+	if (userNumber === randomNumber) {
+		alert("Congrats! You won!");
+		wins++;
+
+	} else if (userNumber > randomNumber) {
+		alert("Better luck next time!");
+		losses++;
+
+	}
+};
+
+		var randNumber = 
+		"<p>" + randomNumber + "</p>";
+
+		document.querySelector('#randNumber').innerHTML = randNumber;
+
+		var uNumber = 
+		"<p>" + userNumber + "</p>";
+
+		document.querySelector('#uNumber').innerHTML = uNumber;
+
+
+		var html = 
+		"<p>Wins: " + wins + "</p>" + 
+		"<p>Losses: " + losses + "</p>";
+
+		document.querySelector("#scoreboard").innerHTML = html;
+ 
+});
+
+
